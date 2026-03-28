e6434ac31ca489be069d53d9549fcdee1760701b
Bennyho2015 <125178782+Bennyho2015@users.noreply.github.com>
2026-03-28 13:05:23 +0800

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                "model": "openai/gpt-4o", // 修改這裡
+                "model": "meta-llama/llama-3.2-90b-vision-instruct", // 修改這裡
                 "messages": [
                     {
                         "role": "user",
