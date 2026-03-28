2f50ed8ff0b966b83701c489a7910cf91604a4b7
Bennyho2015 <125178782+Bennyho2015@users.noreply.github.com>
2026-03-28 16:04:08 +0800

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -21,7 +21,7 @@ export default async function handler(req, res) {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                "model":  "google/gemini-flash-1.5-exp", // 修改這裡
+                "model":  "google/gemini-2.0-flash-001", // 修改這裡
                 "messages": [
                     {
                         "role": "user",
